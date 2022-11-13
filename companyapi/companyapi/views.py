fc34023d02b4ecae24979d1b0020d5d38c738bf8
param <parmanand.122@gmail.com>
2022-11-13 21:16:06 +0530

    sdk-python

--- companyapi/companyapi/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-from django.http import HttpResponse
-
-def home_page(request):
-    print("Home Page requested")
-    return HttpResponse("This is home page")
